1ddfb3887f771b2073b2d1d132e370a582a01d12
Yannick Marcon <yannick.marcon@obiba.org>
2021-04-16 11:50:02 +0000

    version 1.1.2

--- inst/doc/mica-search.R
+++ inst/doc/mica-search.R
@@ -1,26 +1,26 @@
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  library(micar)
 #  m <- mica.login(url="https://mica-demo.obiba.org")
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  mica.networks(m, query="variable(in(Mlstr_area.Lifestyle_behaviours,Drugs))", locale="en", from=0, limit=10)
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  mica.studies(m, query="study(in(Mica_study.methods-design,cohort_study))")
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  mica.study.populations(m, query="study(in(Mica_study.methods-design,cohort_study))")
 #  mica.study.dces(m, query="study(in(Mica_study.methods-design,cohort_study))")
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  mica.datasets(m, query="variable(in(Mlstr_area.Lifestyle_behaviours,Drugs))")
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  mica.variables(m, query="variable(in(Mlstr_area.Lifestyle_behaviours,Drugs))")
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  mica.vocabularies(m, target="variable", query="cancer", locale = "en")
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  mica.logout(m)
 
